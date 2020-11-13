e82577227d49a811f6d68a6a5ddf35c44979f085
Nidheesh Panchal <nilu.1999@yahoo.com>
2020-11-13 12:06:04 +0530

    Reduce notification service startup from the accessibility service.
    Signed-off-by: Nidheesh Panchal <nilu.1999@yahoo.com>

--- app/src/main/java/com/nidheesh/blockit/MyAccessibilityService.java
+++ app/src/main/java/com/nidheesh/blockit/MyAccessibilityService.java
@@ -8,6 +8,7 @@ import android.view.accessibility.AccessibilityEvent;
 public class MyAccessibilityService extends AccessibilityService {
 
 	public static final String TAG = "BlockitLogs";
+	Intent mIntent = new Intent(this, NotService.class);
 
 	@Override
 	public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -15,8 +16,13 @@ public class MyAccessibilityService extends AccessibilityService {
 		if (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
 			Log.d(TAG, "Event notification state changed");
 
-			Log.d(TAG, "Start Notification Listener service.");
-			startService(new Intent(this, NotService.class));
+			String packageName = (String) event.getPackageName();
+			if(packageName.equalsIgnoreCase("com.whatsapp") ||
+			packageName.equalsIgnoreCase("com.instagram"))
+			{
+				Log.d(TAG, "Start Notification Listener service.");
+				startService(mIntent);
+			}
 		}
 	}
 
